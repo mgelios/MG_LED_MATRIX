d82819b48eff9c9dd3d1a75a3082a3bb0872711b
mgelios <antonnadezhkin@gmail.com>
2019-10-14 00:35:01 +0300

    updated code

---
+++ src/fire_effect.h
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <state.h>
+
+short fire_energy[16][16];
+
+CRGB colors[8] = {CRGB::Black, CRGB::Maroon, CRGB::DarkRed, CRGB::Red, CRGB::DarkOrange, CRGB::Orange, CRGB::Gold, CRGB::Yellow};
+
+void fireInit() {
+    memset(fire_energy, 0, sizeof(fire_energy));
+}
+
+void fireAnimation() {
+    for (int j = 15; j > 0; j--) {
+        for (int i = 0; i < 16; i++)
+            if (fire_energy[i][j-1] > 0) {
+                fire_energy[i][j] = fire_energy[i][j-1] - random(fire_energy[i][j-1] / 2 + 1);
+            }            
+        }
+
+    for (int i = 0; i < 16; i++) {
+        fire_energy[i][0] = random(13) + 2;
+    }
+
+    for (int i = 0; i < 16; i++)
+        for (int j = 0; j < 16; j++) {
+            leds[translation[i][j]] = colors[fire_energy[i][j]/2];
+        }
+}

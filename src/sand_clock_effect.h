cf862562955d73b780354ff569774f3c5f557ca3
mgelios <antonnadezhkin@gmail.com>
2019-12-01 22:26:40 +0300

    created file for sandclock effect

---
+++ src/sand_clock_effect.h
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+
+
+
+void initSimplifiedState() {
+
+}
+
+void sandClockSimplifiedAnimation(int offset, CRGB (&leds)[256], unsigned char (&translation)[16][16]) {
+    
+}
